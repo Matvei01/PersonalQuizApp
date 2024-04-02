041b8568d66aab1b75d9ea084b902e8d6b40f5bc
Matvei <khlestov.matvei@mail.ru>
2024-04-02 19:10:04 +0500

    Created base project, removed storyboard references

---
+++ PersonalQuizApp/PersonalQuizApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PersonalQuizApp
+//
+//  Created by Matvei Khlestov on 02.04.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
